7edaf5b5140edcbcf7adee68b2a17cccb6839e17
yousef hamdan <hamdanyousef42@gmail.com>
2023-07-09 14:10:41 -0400

    the basic solar syestem is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarsyStemBasic Omnax=new SolarsyStemBasic();
+        Omnax.Moon="2";
+        Omnax.sun="1";
+        Omnax.Planets="2";
+        Omnax.Stars="4";
+        
+    }
+}
---
+++ src/SolarsyStemBasic.java
@@ -0,0 +1,7 @@
+public class SolarsyStemBasic {
+    String Planets;
+    String Moon;
+    String sun;
+    String Stars;
+
+}
