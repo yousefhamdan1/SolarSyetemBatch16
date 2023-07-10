a144e64c200adb688fb03914630f635bffb06c57
yousef hamdan <hamdanyousef42@gmail.com>
2023-07-10 00:31:40 -0400

    i have aded sun but it has some error to resloved

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Omnax.sun="1";
         Omnax.Planets="2";
         Omnax.Stars="4";
+        FeatureSun Adamx=new FeatureSun();
+        Adamx.name="poya";
+        Adamx.temp=12344231  calcius;
+        Adamx.size="3294km";
         
     }
 }
