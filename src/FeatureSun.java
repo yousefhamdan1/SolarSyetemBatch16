c8f7327cf8055c33a5bd9eef0177e4dc4580fd24
yousef hamdan <hamdanyousef42@gmail.com>
2023-07-09 21:22:26 -0400

    the feature sun has been added sucessfully

---
+++ src/FeatureSun.java
@@ -0,0 +1,5 @@
+public class FeatureSun {
+    String Name;
+    String size;
+    String temp;
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Omnax.sun="1";
         Omnax.Planets="2";
         Omnax.Stars="4";
+        FeatureSun Xeon=new FeatureSun();
+        Xeon.Name="treo";
+        Xeon.size="-32432KM";
+        Xeon.temp="235235 celcius";
         
     }
 }
